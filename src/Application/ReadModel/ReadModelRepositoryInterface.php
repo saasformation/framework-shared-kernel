c36ef614080fdfd4be7ea8d3e17b3be119d05ad0
SaaSFormation <info@saasformation.com>
2024-12-18 07:51:01 +0000

    feat: add mongodb transactions

--- src/Application/ReadModel/ReadModelRepositoryInterface.php
+++ src/Application/ReadModel/ReadModelRepositoryInterface.php
@@ -2,19 +2,21 @@
 
 namespace SaaSFormation\Framework\SharedKernel\Application\ReadModel;
 
+use SaaSFormation\Framework\SharedKernel\Common\Identity\IdInterface;
+
 interface ReadModelRepositoryInterface
 {
-    public function save(AbstractReadModel $readModel): void;
+    public function save(IdInterface $requestId, AbstractReadModel $readModel): void;
 
     /**
      * @param array<string, mixed> $criteria
      * @return ?AbstractReadModel
      */
-    public function findOneByCriteria(array $criteria): ?AbstractReadModel;
+    public function findOneByCriteria(IdInterface $requestId, array $criteria): ?AbstractReadModel;
 
     /**
      * @param array<string, mixed> $criteria
      * @return RepositoryCollectionResult
      */
-    public function findByCriteria(array $criteria): RepositoryCollectionResult;
+    public function findByCriteria(IdInterface $requestId, array $criteria): RepositoryCollectionResult;
 }
